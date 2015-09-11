87c8cce850d15adcb520c10c6852408f7507dc3f
spek <spek@localhost>
2015-09-11 08:46:50 -0600

    - DATA fh template now working with sample $stack aref of hashes data

--- lib/Devel/Trace/Template.pm
+++ lib/Devel/Trace/Template.pm
@@ -2,20 +2,19 @@
 use strict;
 use warnings;
 
+use Data::Dumper;
 use HTML::Template;
+
+#open my $wfh, '>', 'x.html' or die $!;
 
 my @tpl = <DATA>;
 
+my $template = HTML::Template->new(arrayref => \@tpl);
 
-print @tpl;
 
-open my $wfh, '>', 'x.html' or die $!;
-
-print $wfh @tpl;
-
-[
+my $stack = [
     {
-        'called' => 'main::one',
+        'in' => 'main::one',
         'sub' => '-',
         'line' => 8,
         'filename' => 'examples/building.pl',
@@ -26,16 +25,21 @@ print $wfh @tpl;
         'sub' => 'main::one',
         'filename' => 'examples/building.pl',
         'package' => 'main',
-        'called' => 'main::two'
+        'in' => 'main::two'
     },
     {
-        'called' => 'main::three',
+        'in' => 'main::three',
         'sub' => 'main::two',
         'line' => 21,
         'filename' => 'examples/building.pl',
         'package' => 'main'
     }
 ];
+
+#$template->param(stack => $stack);
+
+$template->param(STACK => $stack);
+print $template->output;
 
 1;
 
@@ -62,11 +66,10 @@ __DATA__
 
 <table id=error border=0 cellspacing=0>
 <TMPL_LOOP NAME=STACK>
-        <TMPL_IF sub>
-        <tr><td>method:</td>  <td>&nbsp;</td> <td><TMPL_VAR NAME=sub></td></tr>
-        </TMPL_IF>
-        <tr><td>file:</td>    <td>&nbsp;</td> <td><TMPL_VAR NAME=filename></td></tr>        
-	<tr><td>line:</td>    <td>&nbsp;</td> <td><TMPL_VAR NAME=line></td></tr>
+        <tr><td>in:</td>  <td>&nbsp;</td> <td><TMPL_VAR NAME=in></td></tr>
+        <tr><td>sub:</td>  <td>&nbsp;</td> <td><TMPL_VAR NAME=sub></td></tr>
+        <tr><td>file:</td>    <td>&nbsp;</td> <td><TMPL_VAR NAME=filename></td></tr>
+	    <tr><td>line:</td>    <td>&nbsp;</td> <td><TMPL_VAR NAME=line></td></tr>
         <tr><td>class:</td>   <td>&nbsp;</td> <td><TMPL_VAR NAME=package></td></tr>
         <tr><td colspan=3>&nbsp;</td></tr>
 </TMPL_LOOP>
