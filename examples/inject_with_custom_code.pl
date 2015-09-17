9fdfec1192e0b5092bd6a3b13bdf5cb0932a99a2
Steve Bertrand <steve02@hitachi-id.com>
2015-09-17 08:07:40 -0600

    changes to example file

--- examples/inject_with_custom_code.pl
+++ examples/inject_with_custom_code.pl
@@ -9,7 +9,7 @@ my $remove = $ARGV[0] if $ARGV[0];
 my $inject = ['trace() if $ENV{DTS_ENABLE} && $ENV{DES_TRACE};'];
 
 if (! $remove){
-    install_trace(file => 'devel-examine-subs/lib/Devel/Examine', extensions => ['pm'], inject => $inject);
+    install_trace(file => '../devel-examine-subs/lib/Devel/Examine', extensions => ['pm'], inject => $inject);
 }
 else {
     remove_trace(file => 'devel-examine-subs/lib/Devel/Examine');
